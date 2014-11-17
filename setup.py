d5735098937f9b53dedfb9e06462fe687d2cad74
Carl Osterwisch <costerwi@gmail.com>
2014-11-17 13:49:57 -0500

    Added distutils setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dwdatareader',
+      version='0.1.0',
+      description='Python module to interact with Dewesoft DWDataReaderLib shared library',
+      author='Carl Osterwisch',
+      author_email='costerwi@gmail.com',
+      url='https://github.com/costerwi/dwdatareader/',
+      py_modules=['dwdatareader'],
+      package_data={'dwdatareader': ['DWDataReaderLib.dll', 'DWDataReaderLib64.dll']}
+     )
