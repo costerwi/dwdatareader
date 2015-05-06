eadcbd77886ea693d09b76a485968febd7f88041
Carl Osterwisch <costerwi@gmail.com>
2015-05-06 16:51:40 -0400

    Added README.rst to MANIFEST.in

--- dwdatareader/__init__.py
+++ dwdatareader/__init__.py
@@ -12,7 +12,7 @@ with dw.open('myfile.d7d') as f:
         print(ch.name, ch.series().mean())
 """
 __all__ = ['DWError', 'DWFile', 'getVersion']
-__version__ = '0.7.7'
+__version__ = '0.7.8'
 
 DLL = None # module variable accessible to other classes 
 
