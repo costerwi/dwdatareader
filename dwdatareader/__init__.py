9ba443daccbeb841752a373da0629cb928583602
Carl Osterwisch <costerwi@gmail.com>
2024-11-03 10:07:09 -0500

    Version bump 0.15.5

--- dwdatareader/__init__.py
+++ dwdatareader/__init__.py
@@ -12,7 +12,7 @@ with dw.open('myfile.d7d') as f:
         print(ch.name, ch.series().mean())
 """
 __all__ = ['DWError', 'DWFile', 'getVersion']
-__version__ = '0.15.4'
+__version__ = '0.15.5'
 
 DLL = None # module variable accessible to other classes
 encoding = 'ISO-8859-1'  # default encoding
