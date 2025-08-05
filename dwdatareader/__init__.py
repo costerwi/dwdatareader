cdd978f629a9146fe1329890a6778be3d972677c
Bojan Bogojevic <Bojan.Bogojevic@ProRail.nl>
2025-08-05 10:06:50 +0200

    Cleanup: Remove Mapping import

--- dwdatareader/__init__.py
+++ dwdatareader/__init__.py
@@ -21,7 +21,6 @@ from pathlib import Path
 from typing import Any, List
 from enum import IntEnum
 from xml.etree import ElementTree
-from collections.abc import Mapping
 
 __all__ = ['get_version', 'open_file']
 __version__ = '1.0.0'
